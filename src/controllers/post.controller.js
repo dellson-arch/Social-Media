21b906775c1347732da833700242ddfb899eb6bf
dellson-arch <nayanmahato908@gmail.com>
2025-08-13 11:05:13 -0700

    final backend complete

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,15 +1,31 @@
 const postmodel = require('../models/post.model');
 const generateCaption = require('../service/ai.service');
-
+const uploadfile = require('../service/storage.service');
+const {v4 : uuidv4} = require('uuid')
 async function createPostController(req, res){
     const file = req.file; //req.file ke andar meri image ki puri file ka data aane wala hai 
     console.log("file received:", file);
 
     const base64Image = Buffer.from(file.buffer).toString('base64'); // ye line meri image ko base64 me convert karti hai
-    const caption = await generateCaption(base64Image); // ye line meri image ka caption generate karti hai
 
-    res.json({
-        caption
+
+    // const caption = await generateCaption(base64Image); // ye line meri image ka caption generate karti hai
+    // const result = await uploadfile(file.buffer , `${uuidv4()}` )
+
+    const [caption , result] = await PromiseAll([
+       generateCaption(base64Image),
+       uploadfile(file.buffer , `${uuidv4()}` )
+    ])
+    
+    const post = await postmodel.create({
+        image:result.url,
+        caption:caption ,
+        user: req.user._id
+    })
+
+    res.status(201).json({
+        message: "Post created successfully",
+        post
     })
 }
 
--- src/service/storage.service.js
+++ src/service/storage.service.js
@@ -1,7 +1,18 @@
-var ImageKit = require("imagekit");
+const ImageKit = require("imagekit");
 
-var imagekit = new ImageKit({
-    publicKey : "your_public_api_key",
-    privateKey : "your_private_api_key",
-    urlEndpoint : "https://ik.imagekit.io/your_imagekit_id/"
+const imagekit = new ImageKit({
+    publicKey : process.env.IMAGEKIT_PUBLIC_KEY,
+    privateKey : process.env.IMAGEKIT_PRIVATE_KEY,
+    urlEndpoint : process.env.IMAGEKIT_URL_ENDPOINT
 });
+
+async function uploadfile(file, filename){
+       const response = await imagekit.upload({
+        file: file,
+        fileName : filename,
+        folder:"cohort-ai-social"
+       })
+       return response 
+} 
+
+module.exports = uploadfile
